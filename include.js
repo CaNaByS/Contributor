fa1f4595ba44082b233e08dfce44a26b9a397bb0
DanielO <oancea.daniel98@gmail.com>
2023-03-07 14:22:08 +0200

    Adding animatron project

---
+++ include.js
@@ -0,0 +1,9 @@
+let cards = [
+  {
+    artName: "Animatron",
+    pageLink: "./Contributor/Animatron/index.html",
+    imageLink: "./Contributor/Animatron/Animatron.png",
+    author: "Daniel-Cristian",
+    githubLink: "https://github.com/Daniel-Cristian",
+  },
+];
